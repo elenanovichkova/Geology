a6bd7f67ea9ad8f8e4942f99db7063ed00ed9f71
olganovichkova <58965002+olganovichkova@users.noreply.github.com>
2024-03-03 00:08:00 -0700

    added logo image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Link href="/">Logo Placeholder</Link>
+        <Link href="/" className="relative">
+          {" "}
+          <div className="fixed h-10 w-10 md:h-14 md:w-14  mx-12 mt-1 md:mx-24 md:mt-2">
+            <img src="/Bengal.png" className=""></img>
+          </div>
+        </Link>
         {children}
       </body>
     </html>
